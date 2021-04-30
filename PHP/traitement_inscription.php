a893f462c1e3f70e4d16c3960ca9ddcaefffec21
Marie-Colette <soline@corm.eu>
2021-04-30 17:09:35 +0200

    ajout inscriptio

--- PHP/traitement_inscription.php
+++ PHP/traitement_inscription.php
@@ -2,6 +2,7 @@
 session_start();
 require_once("fonctions_bd.php");
 require_once("fonctions.php");
+require_once("fonctions_affichage.php");
 
 $connexion = connexion("treknet");
 
@@ -9,32 +10,35 @@ $connexion = connexion("treknet");
 if ($_SERVER['REQUEST_METHOD']== "POST"){
     $pseudo= preTraiterChampSQL($_POST['pseudo'],$connexion);
     $email= preTraiterChampSQL($_POST['mail'],$connexion);
-    $mot_de_passe= password_hash(preTraiterChampSQL($_POST['mot_de_passe'],$connexion), PASSWORD_DEFAULT);
+    $mot_de_passe= preTraiterChampSQL(password_hash($_POST['mot_de_passe'], PASSWORD_DEFAULT),$connexion);
     $num_section= preTraiterChampSQL($_POST['section'],$connexion);
+    $espece= preTraiterChampSQL($_POST['espece'],$connexion);
+    $langue= preTraiterChampSQL($_POST['langue'],$connexion);
     $image="../Images/Profil/pp_default.png";
 
-    $requete="SELECT COUNT(*) FROM profil WHERE pseudo = $pseudo OR email=$email";
-    $requete2="SELECT MAX(num_profil) FROM profil";
-    $resultat=requete1($requete,$connexion);
-    $resultat2=requete($requete2,$connexion);
-    $count = $resultat['count(*)'];
-    $num = $resultat2['MAX(num_profil)']+1;
 
-    if($count == 0){
-        $req ="insert into profil (num_profil,pseudo,email,mot_de_passe,num_section,num_grade,photo_de_profil)
-        values ('$num','$pseudo','$email','$mot_de_passe','$num_section',0, '$image')";
-        requete1($req, $connexion);
-
-        
-
-        $req3 = "INSERT INTO abonnement (num_profil_suivi, num_profil_suivant) VALUES ($num,$num)";
-        requete1($req3,$connexion);
-
-        header("Location: index.php");
-        die;
+    if (vide($pseudo,$email,$mot_de_passe) || ($espece ==0) || ($num_section==0) || ($langue==0)){
+        afficher_inscription("Champ(s) Manquant(s)");
+        exit();
     }
-    else{
-        afficher_inscription("Pseudo ou adresse mail déja utilisé(s)");
+
+
+    if (invalidePseudo($pseudo)){
+        afficher_inscription("Le pseudo ne doit contenir que des chiffres et des lettres.");
+        exit();
     }
-}
+
+    if (invalideEmail($email)){
+        afficher_inscription("Email invalide");
+        exit();
+    }
+
+
+    if (compteExiste($connexion,$pseudo,$email)){
+        afficher_inscription("Pseudo ou email déjà utilisé(s)");
+        exit();
+    }
+    creerProfil($pseudo,$email,$mot_de_passe,$num_section,$espece,$langue);    
+} 
+
 ?>

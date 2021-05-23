65c39847460f7181d70086c750a5aed21b95f6d3
Marie-Colette <soline@corm.eu>
2021-05-23 12:05:51 +0200

    nom pp aleatoire et unique

--- PHP/traitement_modifier_profil.php
+++ PHP/traitement_modifier_profil.php
@@ -19,8 +19,9 @@ if(isset($_FILES['photo']) ){
 
             afficher_modifier("Image trop grosse. Taille maximale : 2Mo");
         }else {
+            $numid=rand(1000000000,1111111111111111); /*pour être sûr que deux pp ont un nom different*/
+            $fichier=$dossier.$numid.".".$ext;
             
-            $fichier=$fichier.".".$ext;
         
             if(move_uploaded_file($_FILES['photo']['tmp_name'], $fichier)) {
     
